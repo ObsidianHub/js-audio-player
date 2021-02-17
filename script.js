2cacd7b6c48c1e5f34ef46b427990b9e05f2a484
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-02-17 23:39:51 +0200

    add load, play and pause functional

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const musicContainer = document.getElementById("music_container");
+const musicContainer = document.getElementById("music-container");
 const playBtn = document.getElementById("play");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
@@ -19,4 +19,37 @@ let songIndex = 2;
 loadSong(songs[songIndex]);
 
 // Update song details
-function loadSong(song) {}
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
+// Event listeners
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
